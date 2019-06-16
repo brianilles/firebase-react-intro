7de9a3227bdd2163ceb454b6db2e81d8eaca0e8f
Brian Illes <brianrilles@gmail.com>
2019-06-16 12:41:19 -0700

    Google auth complete

--- firebase-react/src/App.js
+++ firebase-react/src/App.js
@@ -1,12 +1,45 @@
 import React from "react";
 import "./App.css";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">sanity</header>
-    </div>
-  );
+firebase.initializeApp({
+  apiKey: process.env.API_KEY,
+  authDomain: "fir-react-intro.firebaseapp.com"
+});
+
+export default class App extends React.Component {
+  state = { isLoggedIn: false };
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  };
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user });
+      console.log("user", user);
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.isLoggedIn ? (
+          <>
+            <p>Logged in</p>
+            <button onClick={() => firebase.auth().signOut()}>Sign out</button>
+            <p>hello {firebase.auth().currentUser.displayName}</p>
+          </>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
