c6cf52f8156b9cc43e1c7052b725e86efed8d67e
Brian Illes <brianrilles@gmail.com>
2019-06-16 12:55:32 -0700

    UPDATE readme

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./App.css";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+firebase.initializeApp({
+  apiKey: process.env.API_KEY,
+  authDomain: "fir-react-intro.firebaseapp.com"
+});
+
+export default class App extends React.Component {
+  state = { isLoggedIn: false };
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  };
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user });
+      console.log("user", user);
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.isLoggedIn ? (
+          <>
+            <p>Logged in</p>
+            <p>hello {firebase.auth().currentUser.displayName}</p>
+          </>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    );
+  }
+}
